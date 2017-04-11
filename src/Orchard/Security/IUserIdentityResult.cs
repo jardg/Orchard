a3d37ddb0b09f814597f3eff71cfc40992007ccc
jardg <jardg90@gmail.com>
2017-04-11 10:43:09 +0200

    Create IUSerIdentityResult

---
+++ src/Orchard/Security/IUserIdentityResult.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections.Generic;
+
+namespace Orchard.Security {
+    public class IUserIdentityResult {
+
+        public IUserIdentityResult(IUser user) {
+            _user = user;
+            _succeded = true;
+        }
+
+        public IUserIdentityResult(IEnumerable<string> errors) {
+            _errors = errors;
+            _succeded = false;
+        }
+
+        public IUserIdentityResult(string[] errors) {
+            _errors = errors;
+            _succeded = false;
+        }
+
+        private IUser _user;
+        IEnumerable<string> _errors;
+        bool _succeded;
+
+        IEnumerable<string> Errors { get { return _errors; } }
+        bool Succeeded { get { return _succeded; } }
+        IUser User { get { return _user; } }
+    }
+}
